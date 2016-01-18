13225fdbd7b776ab92a69256f9285247a53d23ec
Jani Jegoroff <jani.jegoroff@gmail.com>
2016-01-18 09:31:31 +0000

    Rubocop fixes

--- lib/to_boolean.rb
+++ lib/to_boolean.rb
@@ -2,7 +2,7 @@
 #
 class Object
   def to_boolean
-    value = self.is_a?(String) ? downcase : self
+    value = is_a?(String) ? downcase : self
     [true, 'true', 1, '1'].include?(value)
   end
 end
--- lib/version.rb
+++ lib/version.rb
@@ -1,5 +1,5 @@
 # Gem version
 #
 module ToBoolean
-  VERSION = '1.0.2'
+  VERSION = '1.0.2'.freeze
 end
